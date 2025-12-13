f75389e2d8ecde3fd597a9f53dfbb4c0b51ca3bd
BangKeonwoong <fish9694@gmail.com>
2025-12-13 10:34:06 +0900

    chore(webapp): set Buy Me a Coffee link

--- webapp/lib/config.mjs
+++ webapp/lib/config.mjs
@@ -1,3 +1,3 @@
 // 공개 배포용 설정 (개인정보/비공개 값은 넣지 마세요)
 // Buy Me a Coffee 링크 예: https://www.buymeacoffee.com/yourname
-export const BMC_DONATE_URL = "";
+export const BMC_DONATE_URL = "https://buymeacoffee.com/daniel1005";
